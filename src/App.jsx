6f54abd0d45e9cd543e0daca00d2f4b68cbab5a8
Isaac Perez <terepaimawork@gmail.com>
2026-01-20 16:31:09 +0100

    basic navbar with animation set

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,14 @@
 import React from "react";
+import Navbar from "./sections/Navbar";
+import Hero from "./sections/Hero";
 
 const App = () => {
-  return <div className="container mx-auto max-w-7xl">App</div>
+  return (
+    <div className="container mx-auto max-w-7xl">
+      <Navbar />
+      <Hero />
+    </div>
+  )
 }
 
 export default App;
