ac52c025d034a5b6943c2b47c39ce7b70155d8c3
Isaac Perez <terepaimawork@gmail.com>
2026-01-20 17:40:15 +0100

    finising hero section + three.js model

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,14 @@ const App = () => {
     <div className="container mx-auto max-w-7xl">
       <Navbar />
       <Hero />
+      <section className="min-h-screen"/>
+      <section className="min-h-screen"/>
+      <section className="min-h-screen"/>
+      <section className="min-h-screen"/>
+      <section className="min-h-screen"/>
+      <section className="min-h-screen"/>
+      <section className="min-h-screen"/>
+
     </div>
   )
 }
--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,6 +1,28 @@
+import { Canvas } from "@react-three/fiber"
+import HeroText from "../components/HeroText"
+import ParallaxBg from "../components/ParallaxBg"
+import {Astronaut} from "../components/Astronaut"
+import { OrbitControls } from "@react-three/drei"
+import { useMediaQuery } from "react-responsive"
 const Hero = () => {
+    const isMobile = useMediaQuery({ maxWidth: 853 })
   return (
-    <div>Hero</div>
+    <section className="flex items-start justify-center md:items-start md:justify-start min-h-screen overflow-hidden c-space">
+        <HeroText />
+        <ParallaxBg />
+        <figure className="absolute inset-0" 
+        style={{
+            width: "100vw",
+            height: "100vh"
+        }}>
+            <Canvas camera={{position:[0,1,3]}}>
+                <Astronaut 
+                    scale={isMobile && 0.23} 
+                    position={isMobile && [0, -1.5, 0]}/>
+                <OrbitControls />
+            </Canvas>
+        </figure>
+    </section>
   )
 }
 
