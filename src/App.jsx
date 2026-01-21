932523ac74e535d9907828482393550a1bbb7f13
Isaac Perez <terepaimawork@gmail.com>
2026-01-21 20:50:50 +0100

    Adding timeline

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Navbar from "./sections/Navbar";
 import Hero from "./sections/Hero";
 import About from "./sections/About";
 import Projects from "./sections/Projects";
+import Experiences from "./sections/Experiences";
 
 const App = () => {
   return (
@@ -11,6 +12,7 @@ const App = () => {
       <Hero />
       <About />
       <Projects />
+      <Experiences />
       <section className="min-h-screen"/>
 
     </div>
